13a86bc4083a0d277724bc3f67a5fadadb531e3d
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-09-24 11:47:31 +0044

    Recipe item added

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -9,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
   
   recipes: Recipe[] = [
-      new Recipe('Teste Recipe' , 'This is a simple test recipe' , 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80')
-  ];
+      new Recipe('A Test Recipe' , 'This is a simple test recipe' , 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80')
+     ,
+     new Recipe('A Test Recipe' , 'This is a simple test recipe' , 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80')
+ 
+    ];
 
   constructor() { }
 
