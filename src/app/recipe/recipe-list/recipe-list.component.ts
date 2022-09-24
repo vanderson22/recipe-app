813d6cd545e5390fea0eabb8ace9461b610e241f
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-09-24 11:05:44 +0044

    recipe item static

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
   
-  recipes = [];
+  recipes: Recipe[] = [
+      new Recipe('Teste Recipe' , 'This is a simple test recipe' , 'https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cmVjaXBlfGVufDB8fDB8fA%3D%3D&w=1000&q=80')
+  ];
 
   constructor() { }
 
