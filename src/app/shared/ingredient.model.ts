2cbc5a38652481d488338c5089869661e9b2cd4f
Vanderson Nogueira <vanderson.01@hotmail.com>
2022-09-24 13:42:24 +0044

    Shooping edit form

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+
+
+export class Ingredient {
+
+   
+    /// Não precisa criar as properties dessa forma
+    constructor(public name:string, public amount: number) {   }
+
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+   ingredients: Ingredient[] =[
+         new Ingredient('Apples' , 5), new Ingredient('Tomatoes' , 15), new Ingredient('Pepper' , 5)
+   ];
   constructor() { }
 
   ngOnInit(): void {
